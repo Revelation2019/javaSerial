5053c98d344e240d84d3de19e9bc22ccce5d7660
zhousheng <963435700@qq.com>
2019-07-16 17:26:57 +0800

    javaserial

--- javaserial/src/main/java/demo1/Employee.java
+++ javaserial/src/main/java/demo1/Employee.java
@@ -3,6 +3,7 @@ package demo1;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
+    //反序列化时会根据该类定义的serialVersionUID值、类路径、包路径来判断是否跟反序列化文件中这些值一样，若一样则反序列化，若不一样，则报错
     private static final long serialVersionUID = 1L;
 
     private String name;
